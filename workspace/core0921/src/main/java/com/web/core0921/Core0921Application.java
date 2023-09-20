0ec42fb8c6b5698b75996fdad0b67b92b84d39b3
jeonbyungho <byho.jeon@gmail.com>
2023-09-20 16:57:58 +0900

    다음날 실습용 프로젝트 준비

---
+++ workspace/core0921/src/main/java/com/web/core0921/Core0921Application.java
@@ -0,0 +1,13 @@
+package com.web.core0921;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Core0921Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Core0921Application.class, args);
+	}
+
+}
---
+++ workspace/core0921/src/test/java/com/web/core0921/Core0921ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.core0921;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Core0921ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
