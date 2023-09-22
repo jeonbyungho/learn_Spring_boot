532b841be0eb79e96c8dffbe0b0419cd60cfbfe8
jeonbyungho <byho.jeon@gmail.com>
2023-09-22 09:09:57 +0900

    9월 22일 수업 준비

---
+++ workspace/core0922/src/main/java/com/web/core0922/Core0922Application.java
@@ -0,0 +1,13 @@
+package com.web.core0922;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Core0922Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Core0922Application.class, args);
+	}
+
+}
