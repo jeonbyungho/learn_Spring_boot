c1fcf191b184083337cd790bb30f8a8b018cb9f5
jeonbyungho <byho.jeon@gmail.com>
2023-09-25 16:39:15 +0900

    다음날 준비

---
+++ workspace/core0926/src/main/java/com/web/core0926/Core0926Application.java
@@ -0,0 +1,13 @@
+package com.web.core0926;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Core0926Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Core0926Application.class, args);
+	}
+
+}
---
+++ workspace/core0926/src/test/java/com/web/core0926/Core0926ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.core0926;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Core0926ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
