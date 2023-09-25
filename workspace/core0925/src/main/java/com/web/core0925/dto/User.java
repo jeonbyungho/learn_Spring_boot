bfc90a730a5c77647caba8502662fee6cc21535b
jeonbyungho <byho.jeon@gmail.com>
2023-09-25 13:42:12 +0900

    lombok 생성자 어노테이션 추가

--- workspace/core0925/src/main/java/com/web/core0925/dto/User.java
+++ workspace/core0925/src/main/java/com/web/core0925/dto/User.java
@@ -1,5 +1,6 @@
 package com.web.core0925.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,12 +8,8 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
 public class User {
 	private String name;
 	private int age;
-	public User(String name, int age) {
-		super();
-		this.name = name;
-		this.age = age;
-	}
 }
