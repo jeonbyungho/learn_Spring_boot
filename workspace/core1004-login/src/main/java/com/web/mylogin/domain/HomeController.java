3a17b5e55b34f3ff49ba0bc8a23ba3d7cce27330
jeonbyungho <byho.jeon@gmail.com>
2023-10-04 15:35:44 +0900

    실습 - 불 필요한 import 제거

--- workspace/core1004-login/src/main/java/com/web/mylogin/domain/HomeController.java
+++ workspace/core1004-login/src/main/java/com/web/mylogin/domain/HomeController.java
@@ -1,20 +1,9 @@
 package com.web.mylogin.domain;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.web.mylogin.domain.member.Member;
-import com.web.mylogin.domain.member.MemberRepository;
-
-import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequiredArgsConstructor
 public class HomeController {
-   
-   private final MemberRepository memberRepository;
 
    @Deprecated
    //@GetMapping("/")
