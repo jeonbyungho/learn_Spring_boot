61be29f865727723af2edcc5bd9a0573d8c003ae
jeonbyungho <byho.jeon@gmail.com>
2023-10-04 12:50:36 +0900

    실습 - 쿠키로 로그인 유지

--- workspace/core1004-login/src/main/java/com/web/mylogin/domain/HomeController.java
+++ workspace/core1004-login/src/main/java/com/web/mylogin/domain/HomeController.java
@@ -1,13 +1,44 @@
 package com.web.mylogin.domain;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.web.mylogin.domain.member.Member;
+import com.web.mylogin.domain.member.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
    
-   @RequestMapping("/")
+   private final MemberRepository memberRepository;
+
+   @Deprecated
+   //@GetMapping("/")
    public String home(){
       return "home";
+   }
+
+   /*
+    * name = memberId -> 쿠키의 Key값으로 Get 매핑을 한다.
+    * required = false -> memberId가 필수 값이 아니므로, 로그인 안한 상태로 접속이 가능하다.
+    */
+   @GetMapping("/")
+   public String homeLogin(
+         @CookieValue(name = "memberId", required = false)Long memberId,
+         Model model){
+      // 로그인 상태가 아니라면 home으로 return.
+      if(memberId == null) return "home";
+
+      // DB 조회 한 후, 사용자가 없으면 null 처리하고 home을 return.
+      Member loginMember = memberRepository.findById(memberId);
+      if(loginMember == null) return "home";
+
+      // 로그인 성공 loginHome을 return.
+      model.addAttribute("member", loginMember);
+      return "loginHome";
    }
 }
--- workspace/core1004-login/src/main/java/com/web/mylogin/domain/login/LoginController.java
+++ workspace/core1004-login/src/main/java/com/web/mylogin/domain/login/LoginController.java
@@ -1,10 +1,16 @@
 package com.web.mylogin.domain.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.web.mylogin.domain.member.Member;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -19,10 +25,22 @@ public class LoginController {
    }
 
    @PostMapping("/login")
-   public String loginMember(@ModelAttribute LoginForm loginForm){
+   public String loginMember(
+         @ModelAttribute LoginForm loginForm, 
+         Model model, RedirectAttributes redirect, 
+         HttpServletResponse resp){
       String loginId = loginForm.getLoginId();
       String password = loginForm.getPassword();
-      loginService.login(loginId, password);
+      
+      Member loginMember = loginService.login(loginId, password);
+      if(loginMember == null) {
+         model.addAttribute("message", "로그인 실패");
+         return "login";
+      }
+
+      Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+      resp.addCookie(idCookie);
+      redirect.addAttribute("message", "로그인 성공");
       return "redirect:/";
    }
 }
